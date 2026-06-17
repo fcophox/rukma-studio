8d50ac576dbbb1e08e0a610b49e1d95ed93e9f5a
fcophox <fcojhormazabalh@gmail.com>
2026-06-17 17:22:17 -0400

    homologar avatares de los formularios sin espaciado interno

--- src/components/contacto/MeetingForm.tsx
+++ src/components/contacto/MeetingForm.tsx
@@ -285,7 +285,7 @@ export function MeetingForm() {
       <div className="bg-[#1A1D21] rounded-xl p-4 flex items-center gap-4">
         <div className="relative w-10 h-10 shrink-0">
           <div className="w-full h-full rounded-full overflow-hidden relative bg-[#0D0F12]">
-            <Image src="/icon.svg" alt="Rukma" fill className="object-cover p-1" />
+            <Image src="/icon.svg" alt="Rukma" fill className="object-cover" />
           </div>
           <div className="absolute -bottom-0.5 -right-0.5 w-3.5 h-3.5 bg-green-500 border-2 border-[#1A1D21] rounded-full z-10"></div>
         </div>
